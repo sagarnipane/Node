ffe38297b5a0435201f31d17b7769906f16c1ebf
Sagar Nipane <spnipane@gmail.com>
2019-02-28 23:45:40 +0530

    Reading JSON file

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 //Server.js File
 var express=require('express');
 var app=express();
+var jsondata=require("./data.json");
+//Route for root
 app.get('/',function(req,res){
     res.writeHead(200,{"Content-Type":"text/html"});
+    res.write("Hello Node.js");
     res.end("\n\n End");
+});
+
+// Routing for getting JSON Data
+app.get('/json',(req,res)=>{
+    res.writeHead(200,{"Content-Type":"text/html"});
+    res.write(JSON.stringify(jsondata));
+    res.end("<br/>End of JSON Data");
 });
 app.listen(3000);
