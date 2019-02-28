a7adc92c398288ac98d073606e79a847d2fe020d
Sagar Nipane <spnipane@gmail.com>
2019-02-28 23:24:35 +0530

    Initial Commit1

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 //Server.js File
+var express=require('express');
+var app=express();
+app.get('/',function(req,res){
+    res.writeHead(200,{"Content-Type":"text/html"});
+    res.end("\n\n End");
+});
+app.listen(3000);
