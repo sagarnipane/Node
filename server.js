1f08f7a6d44982c6ce7a711eda30b77410c86841
sagar.nipane <spnipane@gmail.com>
2019-08-07 17:05:23 +0530

    setting up environment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express=require('express');
 var app=express();
 var jsondata=require("./data/data.json");
+var env=require("./environment.json")
 //Route for root
 app.get('/',function(req,res){
     res.writeHead(200,{"Content-Type":"text/html"});
@@ -11,9 +12,10 @@ app.get('/',function(req,res){
 
 // Routing for getting JSON Data
 app.get('/json',(req,res)=>{
+    connectionString="mongodb+srv://admin:"+env.mongoPassword+"@cluster0-a9wm1.mongodb.net/test?retryWrites=true&w=majority"
     res.writeHead(200,{"Content-Type":"application/json"});
     res.write(JSON.stringify(jsondata));
-    res.end("<br/>End of JSON Data");
+    res.end(`<br/>${connectionString}`);
 });
 app.listen(3000,()=>{
     console.log('Server listening at Port : 3000');
